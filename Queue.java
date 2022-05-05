2cae965cce20e16ce84adb313f8e526ff1280369
Santharao <70807646+Santharaojinaga@users.noreply.github.com>
2022-05-05 17:19:14 +0530

    Collections

---
+++ Queue.java
@@ -0,0 +1,63 @@
+/*import java.util.*;//PriourityQueeue
+class Queue
+  {
+    public static void main(String args[])
+    {
+      PriorityQueue<String> pq= new PriorityQueue<String>();
+      pq.add("XYZ");
+       pq.add("lms");
+       pq.add("abs");
+       pq.add("rds");
+      System.out.println(pq);
+    }
+  }*/
+
+/*import java.util.*;//Hash set
+class Queue
+  {
+    public static void main(String args[])
+    {
+      HashSet<String> pq= new HashSet<String>();
+      pq.add("XYZ");
+       pq.add("lms");
+       pq.add("abs");
+       pq.add("rds");
+       pq.add(null);
+      System.out.println(pq);
+    }
+  }
+*/
+
+/*import java.util.*;//Tree set
+class Queue
+  {
+    public static void main(String args[])
+    {
+      TreeSet<String> pq= new TreeSet<String>();
+      pq.add("XYZ");
+       pq.add("lms");
+       pq.add("abs");
+       pq.add("rds");
+      
+      System.out.println(pq);
+    }
+  }
+*/
+
+
+import java.util.*;//Array List
+class Queue
+  {
+    public static void main(String args[])
+    {
+      ArrayList<Integer> pq= new ArrayList<Integer>();
+      pq.add(123);
+       pq.add(11);
+       pq.add(456);
+       pq.add(679);
+      pq.add(679);
+      pq.add(null);
+      System.out.println(pq);
+    }
+  }
+
